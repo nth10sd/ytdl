4cad4e41a2ca99d1f4cd98d7da05665ebbad9c62
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-08-04 17:01:46 -0700

    Fix linter errors.

--- start.sh
+++ start.sh
@@ -15,7 +15,8 @@ pip install --user --upgrade "$1";
 echo "Creating config directory...";
 mkdir -p "$HOME/.config/$1/";
 echo "Retrieving config script...";
-curl --proto '=https' --tlsv1.2 -sSf -o "$HOME/.config/$1/config" https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/config
+configurl="https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/config"
+curl --proto '=https' --tlsv1.2 -sSf -o "$HOME/.config/$1/config" "$configurl";
 
 echo "Creating destination directories...";
 mkdir -p "$HOME/storage/downloads/Youtube/Songs";
@@ -25,7 +26,8 @@ echo "Creating ~/bin directory...";
 mkdir "$HOME/bin";
 echo "Retrieving termux-url-opener script...";
 urlopener="$HOME/bin/termux-url-opener"
-curl --proto '=https' --tlsv1.2 -sSf -o "$urlopener" https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/termux-url-opener
+x="https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/termux-url-opener"
+curl --proto '=https' --tlsv1.2 -sSf -o "$urlopener" "$x";
 if [[ -f "$urlopener" ]]; then
     echo "Replacing placeholder with intended program...";
     sed "s/PLACEHOLDERNAME/$1/g" "$urlopener";
