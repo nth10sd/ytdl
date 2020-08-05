eacf95ead5de15ac36128b2f1d1de9b7107d18d6
Gary Kwong [:gkw] <nth10sd@gmail.com>
2020-08-04 18:52:08 -0700

    Tweak some commands and their locations in start.sh and termux-url-opener.

--- start.sh
+++ start.sh
@@ -5,23 +5,13 @@ pkg update -y;
 echo "[ytdl] Installing needed packages...";
 pkg install --no-install-recommends -y ffmpeg python;
 
-echo "[ytdl] Installing pip and setuptools...";
+echo "[ytdl] Installing latest versions of pip and setuptools...";
 pip install --user --upgrade pip setuptools;
 echo "[ytdl] Installing $1...";
 pip install --user --upgrade "$1";
 
-echo "Creating config directory...";
-mkdir -p "$HOME/.config/$1/";
-echo "Retrieving config script...";
-configurl="https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/config"
-curl --proto '=https' --tlsv1.2 -sSf -o "$HOME/.config/$1/config" "$configurl";
-
-echo "Creating destination directories...";
-mkdir -p "$HOME/storage/downloads/Youtube/Songs";
-mkdir -p "$HOME/storage/downloads/Youtube/Videos";
-
-echo "Creating ~/bin directory...";
-mkdir "$HOME/bin";
+echo "Creating ~/bin directory if it does not yet exist...";
+mkdir -p "$HOME/bin/";
 echo "Retrieving termux-url-opener script...";
 urlopener="$HOME/bin/termux-url-opener"
 x="https://raw.githubusercontent.com/nth10sd/ytdl/master/cfg/termux-url-opener"
